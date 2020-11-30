2a87ce8b6ae43067cc301349736f226c49e51d44
onTheDL <daniel.laserna@protonmail.com>
2020-11-29 21:37:49 -0500

    Create repositories grid

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,4 +1,5 @@
 export function fetchPopularRepos (language) {
+  
   const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
 
   return fetch(endpoint)
